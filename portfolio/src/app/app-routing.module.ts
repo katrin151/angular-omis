ecb456f4d769040d5b8e4b73d99255b33dc22465
katrin151 <katrinvs3@gmail.com>
2020-08-10 17:35:22 +0300

    portfolio leht, firebase

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { WorkComponent } from './work/work.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { CoursesComponent } from './courses/courses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'', component: MainComponent },
+{path:'work', component: WorkComponent },
+{path:'hobbies', component: HobbiesComponent },
+{path:'courses', component: CoursesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { WorkComponent } from './work/work.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    WorkComponent,
+    HobbiesComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
