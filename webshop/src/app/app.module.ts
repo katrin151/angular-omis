8a308308ee6beb84dd40808431ae9cbcb27b1d18
katrin151 <katrinvs3@gmail.com>
2020-08-17 19:25:25 +0300

    generated all komponents

---
+++ webshop/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './global/menu/menu.component';
+import { FooterComponent } from './global/footer/footer.component';
+import { HeaderComponent } from './global/header/header.component';
+import { NotFoundComponent } from './global/not-found/not-found.component';
+import { ItemComponent } from './item/item.component';
+import { ItemListComponent } from './item/item-list/item-list.component';
+import { ItemViewComponent } from './item/item-view/item-view.component';
+import { ItemNewComponent } from './item/item-new/item-new.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { SignupComponent } from './authentication/signup/signup.component';
+import { CartComponent } from './cart/cart.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HeaderComponent,
+    NotFoundComponent,
+    ItemComponent,
+    ItemListComponent,
+    ItemViewComponent,
+    ItemNewComponent,
+    AuthenticationComponent,
+    LoginComponent,
+    SignupComponent,
+    CartComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
