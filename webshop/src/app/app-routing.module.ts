071747cb435f22caf7b7dfd00a0ca79dbc030ccd
katrin151 <katrinvs3@gmail.com>
2020-08-17 20:34:04 +0300

    header tehtud esmasp

--- webshop/src/app/app-routing.module.ts
+++ webshop/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ItemListComponent } from './item/item-list/item-list.component';
+import { ItemNewComponent } from './item/item-new/item-new.component';
+import { ItemViewComponent } from './item/item-view/item-view.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { SignupComponent } from './authentication/signup/signup.component';
+import { CartComponent } from './cart/cart.component';
+import { NotFoundComponent } from './global/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  { path: '', component: ItemListComponent},
+  { path: 'item/new', component: ItemNewComponent},
+  { path: 'item/:id', component: ItemViewComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'signup', component: SignupComponent},
+  { path: 'registrate', redirectTo: "signup"},
+  { path: 'cart', component: CartComponent},
+  { path: '**', component: NotFoundComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
