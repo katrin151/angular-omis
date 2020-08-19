52a91a1c7a8343c2950432fb9870630933356f25
katrin151 <katrinvs3@gmail.com>
2020-08-19 20:44:23 +0300

    signin

--- webshop/src/app/authentication/login/login.component.ts
+++ webshop/src/app/authentication/login/login.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  isSignUp = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onButtonClick() {
+    this.isSignUp =!this.isSignUp;
+
   }
 
 }
