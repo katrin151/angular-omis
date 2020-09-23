ea025b469ed478248118de5aa87e98041308d4d4
katrin151 <katrinvs3@gmail.com>
2020-09-23 20:43:42 +0300

    input

--- reactivity/src/app/app.component.ts
+++ reactivity/src/app/app.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'reactivity';
   muutuja;
+
+  likeMuutuja = false;
+
+  onKlikk() {
+    this.likeMuutuja = !this.likeMuutuja;
+  }
 }
