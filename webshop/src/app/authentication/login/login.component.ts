1b6645266851d3473fcb87ebdd433bf1b13a60d9
katrin151 <katrinvs3@gmail.com>
2020-09-28 20:41:30 +0300

    favicon flags login

--- webshop/src/app/authentication/login/login.component.ts
+++ webshop/src/app/authentication/login/login.component.ts
@@ -1,37 +1,70 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService, AuthResponseData} from '../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
   isSignUp = true;
+  isLoading = false;
+  error: string = null;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private authService: AuthService
+  ) { }
 
-  ngOnInit(): void {
-  }
-
-  onButtonClick() {
+    onButtonClick() {
     this.isSignUp =!this.isSignUp;
-
   }
-  onSubmit(form: NgForm) {
-    console.log(form);
-    console.log(form.value);
-    /*console.log(form.invalid);
-    console.log(form.value.email);
-    
-    console.log(form["invalid"]);
-    console,log(form["vkvkldld"]);*/
+  
+  ngOnInit(): void {
+    this.authService.autoLogin();
   }
 
-  onSignup(form: NgForm) {
-    console.log(form);
-    console.log(form.value);
+ onLogin(loginForm: NgForm) {
+    if (!loginForm.valid) {
+      return;
+    }
+    this.isLoading = true;
+    let authObs: Observable<AuthResponseData>;
+    authObs = this.authService.login(loginForm.value.username, loginForm.value.password);
+    authObs.subscribe(
+      resData => {
+        this.isLoading = false;
+        this.router.navigate(["/item/all"], { relativeTo: this.route });
+                /*this.router.navigate(["projects"], { relativeTo: this.route });*/
+      },
+      errorMessage => {
+        this.error = errorMessage;
+        this.isLoading = false;
+      }
+    );
+    loginForm.reset();
+  }
+
+  onSignup(signUpForm: NgForm) {
+    if (!signUpForm.valid) {
+      return;
+    }
+    this.isLoading = true;
+    let authObs: Observable<AuthResponseData>;
+    authObs = this.authService.signUp(signUpForm.value.username, signUpForm.value.password);
+    authObs.subscribe(resData => {
+        this.isLoading = false;
+    }, error => {
+      this.error = error;
+      this.isLoading = false;
+    });
+    signUpForm.reset();
 
   }
 
-}
+  }
