055f116ee2537290f61c0b8aa73fbd3790e7eb2d
katrin151 <katrinvs3@gmail.com>
2020-09-28 16:40:13 +0300

    service asukoht teine

---
+++ webshop3/src/app/item-edit/item-edit.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ItemService } from '../item.service';
+
+@Component({
+  selector: 'app-item-edit',
+  templateUrl: './item-edit.component.html',
+  styleUrls: ['./item-edit.component.css']
+})
+export class ItemEditComponent implements OnInit {
+  itemId;
+  item;
+  itemEditForm;
+
+  constructor(private route: ActivatedRoute,
+      private itemService: ItemService,
+      private router: Router) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(param => {
+      this.itemId = param.id;
+    });
+    this.item = this.itemService.getItem(this.itemId);
+    this.itemEditForm = new FormGroup({
+      imgSrc: new FormControl(this.item.imgSrc),
+      title: new FormControl(this.item.title),
+      price: new FormControl(this.item.price),
+      category: new FormControl(this.item.category),
+    });
+  }
+
+  onSubmit() {
+    this.itemService.editItem(
+      this.itemId,
+      this.itemEditForm.value
+    );
+    this.router.navigateByUrl("/");
+  }
+}
---
+++ webshop3/src/app/item-list/item-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from './../item.service';
+
+@Component({
+  selector: 'app-item-list',
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.css']
+})
+export class ItemListComponent implements OnInit {
+  items;
+  constructor(private itemService: ItemService) { }
+
+  ngOnInit(): void {
+    this.items = this.itemService.getItems();
+  }
+
+}
